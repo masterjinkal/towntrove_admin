f38e6884919a37c4559dbed26311e6dd90d08839
Jinkal Master <masterjinkal123@gmail.com>
2024-04-27 01:07:47 +0530

    Modified

--- src/Components/Navbar/Navbar.jsx
+++ src/Components/Navbar/Navbar.jsx
@@ -1,12 +1,13 @@
 import React from 'react'
 import './Navbar.css'
-import navlogo from '../../assets/nav-logo.svg'
-import navProfile from '../../assets/nav-profile.svg'
+import navlogo from '../../assets/nav_logo.png'
+import navProfile from '../../assets/nav_profile.jpg'
 
 const Navbar = () => {
   return (
     <div className='navbar'>
         <img className='nav-logo' src={navlogo} alt="" />
+        <p>Admin Panel</p>
         <img src={navProfile} className='nav-Profile' alt="" />
     </div>
   )
